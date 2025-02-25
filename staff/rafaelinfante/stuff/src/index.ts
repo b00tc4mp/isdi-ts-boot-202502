d3ab60819b77a58b3c301ea8054ad2f83c91e2e8
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2025-02-25 21:05:28 +0100

    install ts files #3

---
+++ staff/rafaelinfante/stuff/src/index.ts
@@ -0,0 +1,59 @@
+const age = 34
+
+const name1 = 'Peter'
+
+let surname = 'Parker'
+
+const numbers: number[] = [2, 4, 6]
+
+function sumNumbers(numbers: number[]): number {
+  return numbers.reduce((total, number) => {
+    total += number
+    return total
+  }, 0)
+}
+
+const numbersSum = sumNumbers(numbers)
+
+// interface Person {
+//   name: string
+//   age: number
+// }
+
+// interface Student extends Person {
+//   score: number
+// }
+
+type Student = {
+  name: string
+  age: number
+  score: number | string // Union type
+}
+
+const students: Student[] = [
+  {
+    name: 'Pep',
+    age: 43,
+    score: '90',
+  },
+  {
+    name: 'Josep',
+    age: 34,
+    score: 70,
+  },
+  {
+    name: 'Anna',
+    age: 25,
+    score: 80,
+  },
+]
+
+function triplify(value: number | string): number | string {
+  if (typeof value === 'number') {
+    return value * 3
+  }
+
+  return value.repeat(3)
+}
+
+// console.log(triplify('a'))
